31362c44d34c8e53a40f51b3b49c91ba99ac0d1b
oleg <msoftorudnev@gmail.com>
2026-02-13 17:07:06 +0600

    Добавил метод ReceiveAllRowsFromCloud

--- src/helpers/webApiWrapper.ts
+++ src/helpers/webApiWrapper.ts
@@ -1,20 +1,8 @@
 import { loadAllPhrases, SpItem } from "../db/speechDb";
 
-const API_URL = "https://script.google.com/macros/s/AKfycbxhGEMHBsJAuJmpVaZZN0sxu5VTcnX2XE_TOI005QgLoP0NcENfFMZFQ9wWtkoZ99f8-w/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbwS8blQc2ycTE2m4bLFKFmlEbHBmTRlWTXPWcvZCwcMOBobTFjG6ERxDtlolrw6Nrv3Cg/exec";
 
 
-export async function SendDatabaseToCloud_test() {
-  let items = await loadAllPhrases();
-  //   let payload = [{
-  //   uid:"QUTQJ7PQ",
-  //   variants:"blablabla",
-  //   tsr:55
-  // }];
-  let payload: any = items.map(r => {
-    return { uid: r.uid, topic: r.topic, q: r.q, a: r.a, variants: r.variants }
-  });
-  SendDatabaseToCloud(payload);
-}
 
 
 export async function SendDatabaseToCloud(Payload: any) {
@@ -35,4 +23,17 @@ export async function SendDatabaseToCloud(Payload: any) {
 
   const data = await response.json();
   console.log(data);
+}
+
+export async function ReceiveAllRowsFromCloud() {
+  const response = await fetch(API_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      methodName: "GetAllRows",
+    }),
+  });
+  return response;
 }
