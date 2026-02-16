8806df953617bbdeca8ef9b6da459ce00a284e9b
oleg <msoftorudnev@gmail.com>
2026-02-16 13:02:57 +0600

    структура данных AppSettings

--- src/db/settings.ts
+++ src/db/settings.ts
@@ -1,3 +1,29 @@
-type TSettings = {
-    
+export type TSettingName = 'fullAccess'|'reverseMode'|'rowsCloudDataSource'|'selectedTopics';
+export type TServiceCommandName = 'downloadRowsFromCloud'|'uploadRowsToCloud';
+
+export type TsettingsItem = {
+    name: TSettingName;
+    value?: any;
+    defaultValue: any;
+    needAdvancedRights?: boolean;
 }
+
+export const AppSettings: TsettingsItem[] = [
+    {
+        name: 'fullAccess',
+        defaultValue: false,
+    },   
+    {
+        name: 'reverseMode',
+        defaultValue: false,
+    },
+    {
+        name: 'selectedTopics',
+        defaultValue: [],
+    },
+    {
+        name: 'rowsCloudDataSource',
+        defaultValue: '',
+        needAdvancedRights: true,
+    }
+]
